b27c8fa5069ec5764722da7caf9cd1d216db6c1c
Timur Badretdinov <accounts@destiner.io>
2024-04-16 20:35:15 +0100

    Add RootValidatorUpdated event handler

--- src/kernelV3Account.ts
+++ src/kernelV3Account.ts
@@ -1,5 +1,38 @@
 import { KernelV3AccountContract } from "../generated/src/Handlers.gen";
 
+const TYPE_VALIDATOR = "0x01";
+const TYPE_PERMISSION = "0x02";
+
+KernelV3AccountContract.RootValidatorUpdated.loader(({ event, context }) => {
+  const account = event.srcAddress.toLowerCase();
+  context.KernelV3Account.load(`${event.chainId}-${account}`);
+});
+
+KernelV3AccountContract.RootValidatorUpdated.handler(({ event, context }) => {
+  const accountAddress = event.srcAddress.toLowerCase();
+  const account = context.KernelV3Account.get(
+    `${event.chainId}-${accountAddress}`
+  );
+  if (!account) {
+    return;
+  }
+  const rootValidator = event.params.rootValidator.toLowerCase();
+  const validatorType = rootValidator.slice(0, 4);
+  if (validatorType === TYPE_VALIDATOR) {
+    const validatorAddress = `0x${rootValidator.slice(4, 44)}`;
+    context.KernelV3Account.set({
+      id: `${event.chainId}-${accountAddress}`,
+      modules: account.modules,
+      chainId: account.chainId,
+      rootValidator: validatorAddress,
+      address: account.address,
+    });
+  } else if (validatorType === TYPE_PERMISSION) {
+    // TODO
+    return;
+  }
+});
+
 KernelV3AccountContract.ModuleInstalled.loader(({ event, context }) => {
   const account = event.srcAddress.toLowerCase();
   context.KernelV3Account.load(`${event.chainId}-${account}`);
