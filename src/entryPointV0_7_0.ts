e9753d16e1a7eea0f3a75e0670d05173dcb8a8cc
Timur Badretdinov <accounts@destiner.io>
2024-04-16 18:38:08 +0100

    Initial commit

---
+++ src/entryPointV0_7_0.ts
@@ -0,0 +1,25 @@
+import { EntryPointV0_7_0Contract } from "../generated/src/Handlers.gen";
+
+const KERNEL_V3_FACTORY_ADDRESS = "0xd703aae79538628d27099b8c4f621be4ccd142d5";
+
+EntryPointV0_7_0Contract.AccountDeployed.loader(({ event, context }) => {
+  const address = event.params.sender;
+  const factory = event.params.factory;
+  if (factory === KERNEL_V3_FACTORY_ADDRESS) {
+    context.KernelV3Account.load(`${event.chainId}-${address}`);
+    context.contractRegistration.addKernelV3Account(address);
+  }
+});
+
+EntryPointV0_7_0Contract.AccountDeployed.handler(({ event, context }) => {
+  const address = event.params.sender;
+  const factory = event.params.factory;
+  if (factory === KERNEL_V3_FACTORY_ADDRESS) {
+    context.KernelV3Account.set({
+      id: `${event.chainId}-${address}`,
+      chainId: event.chainId,
+      address,
+      modules: [],
+    });
+  }
+});
