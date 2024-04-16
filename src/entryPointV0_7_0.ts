6cde7ac43dfa959693935f71d6b3843c8e1afee2
Timur Badretdinov <accounts@destiner.io>
2024-04-16 19:49:31 +0100

    Lowercase all addresses

--- src/entryPointV0_7_0.ts
+++ src/entryPointV0_7_0.ts
@@ -1,10 +1,10 @@
 import { EntryPointV0_7_0Contract } from "../generated/src/Handlers.gen";
 
-const KERNEL_V3_FACTORY_ADDRESS = "0xd703aaE79538628d27099B8c4f621bE4CCd142d5";
+const KERNEL_V3_FACTORY_ADDRESS = "0xd703aae79538628d27099b8c4f621be4ccd142d5";
 
 EntryPointV0_7_0Contract.AccountDeployed.loader(({ event, context }) => {
-  const address = event.params.sender;
-  const factory = event.params.factory;
+  const address = event.params.sender.toLowerCase();
+  const factory = event.params.factory.toLowerCase();
   if (factory === KERNEL_V3_FACTORY_ADDRESS) {
     context.KernelV3Account.load(`${event.chainId}-${address}`);
     context.contractRegistration.addKernelV3Account(address);
@@ -12,8 +12,8 @@ EntryPointV0_7_0Contract.AccountDeployed.loader(({ event, context }) => {
 });
 
 EntryPointV0_7_0Contract.AccountDeployed.handler(({ event, context }) => {
-  const address = event.params.sender;
-  const factory = event.params.factory;
+  const address = event.params.sender.toLowerCase();
+  const factory = event.params.factory.toLowerCase();
   if (factory === KERNEL_V3_FACTORY_ADDRESS) {
     context.KernelV3Account.set({
       id: `${event.chainId}-${address}`,
