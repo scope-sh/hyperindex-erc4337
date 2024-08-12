851e634db66b0b642710dbeedfcf81021823ac0d
Timur Badretdinov <accounts@destiner.io>
2024-08-12 12:22:55 +0100

    Migrate to envio 2

--- src/entryPoint.ts
+++ src/entryPoint.ts
@@ -1,50 +1,43 @@
 import {
-  EntryPointV0_6_0Contract,
-  EntryPointV0_7_0Contract,
-} from "../generated/src/Handlers.gen";
-import {
-  EntryPointV0_6_0Contract_AccountDeployedEvent_eventArgs,
-  EntryPointV0_6_0Contract_AccountDeployedEvent_handlerContext,
-  EntryPointV0_6_0Contract_AccountDeployedEvent_loaderContext,
-  EntryPointV0_6_0Contract_UserOperationEventEvent_eventArgs,
-  EntryPointV0_6_0Contract_UserOperationEventEvent_handlerContext,
-  EntryPointV0_6_0Contract_UserOperationEventEvent_loaderContext,
+  contractRegistrations,
+  EntryPointV0_6_0,
+  EntryPointV0_6_0_AccountDeployed_eventArgs,
+  EntryPointV0_6_0_UserOperationEvent_eventArgs,
+  EntryPointV0_7_0,
   eventLog,
-} from "../generated/src/Types.gen";
+  handlerContext,
+} from "generated";
 
 const KERNEL_V3_FACTORY_ADDRESS = "0xd703aae79538628d27099b8c4f621be4ccd142d5";
 
-EntryPointV0_6_0Contract.AccountDeployed.loader(({ event, context }) => {
-  loadAccountDeployed(event, context);
+EntryPointV0_6_0.AccountDeployed.contractRegister(({ event, context }) => {
+  handleAccountDeployedContractRegister(event, context);
+});
+EntryPointV0_7_0.AccountDeployed.contractRegister(({ event, context }) => {
+  handleAccountDeployedContractRegister(event, context);
 });
 
-EntryPointV0_7_0Contract.AccountDeployed.loader(({ event, context }) => {
-  loadAccountDeployed(event, context);
-});
-
-function loadAccountDeployed(
-  event: eventLog<EntryPointV0_6_0Contract_AccountDeployedEvent_eventArgs>,
-  context: EntryPointV0_6_0Contract_AccountDeployedEvent_loaderContext
+function handleAccountDeployedContractRegister(
+  event: eventLog<EntryPointV0_6_0_AccountDeployed_eventArgs>,
+  context: contractRegistrations
 ) {
   const address = event.params.sender.toLowerCase();
   const factory = event.params.factory.toLowerCase();
   if (factory === KERNEL_V3_FACTORY_ADDRESS) {
-    context.KernelV3Account.load(`${event.chainId}-${address}`);
-    context.contractRegistration.addKernelV3Account(address);
+    context.addKernelV3Account(address);
   }
 }
 
-EntryPointV0_6_0Contract.AccountDeployed.handler(({ event, context }) => {
-  handleAccountDeployed(event, context);
+EntryPointV0_6_0.AccountDeployed.handler(async ({ event, context }) => {
+  handleAccountDeployedEvent(event, context);
+});
+EntryPointV0_7_0.AccountDeployed.handler(async ({ event, context }) => {
+  handleAccountDeployedEvent(event, context);
 });
 
-EntryPointV0_7_0Contract.AccountDeployed.handler(({ event, context }) => {
-  handleAccountDeployed(event, context);
-});
-
-function handleAccountDeployed(
-  event: eventLog<EntryPointV0_6_0Contract_AccountDeployedEvent_eventArgs>,
-  context: EntryPointV0_6_0Contract_AccountDeployedEvent_handlerContext
+async function handleAccountDeployedEvent(
+  event: eventLog<EntryPointV0_6_0_AccountDeployed_eventArgs>,
+  context: handlerContext
 ) {
   const address = event.params.sender.toLowerCase();
   const factory = event.params.factory.toLowerCase();
@@ -59,44 +52,34 @@ function handleAccountDeployed(
   }
 }
 
-EntryPointV0_6_0Contract.UserOperationEvent.loader(({ event, context }) => {
-  loadUserOperationEvent(event, context);
-});
-
-EntryPointV0_7_0Contract.UserOperationEvent.loader(({ event, context }) => {
-  loadUserOperationEvent(event, context);
-});
-
-function loadUserOperationEvent(
-  event: eventLog<EntryPointV0_6_0Contract_UserOperationEventEvent_eventArgs>,
-  context: EntryPointV0_6_0Contract_UserOperationEventEvent_loaderContext
-) {
-  const hash = event.params.userOpHash;
-  context.UserOp.load(`${event.chainId}-${hash}`);
-}
-
-EntryPointV0_6_0Contract.UserOperationEvent.handler(({ event, context }) => {
+EntryPointV0_6_0.UserOperationEvent.handler(async ({ event, context }) => {
   handleUserOperationEvent(event, context);
 });
 
-EntryPointV0_7_0Contract.UserOperationEvent.handler(({ event, context }) => {
+EntryPointV0_7_0.UserOperationEvent.handler(async ({ event, context }) => {
   handleUserOperationEvent(event, context);
 });
 
-function handleUserOperationEvent(
-  event: eventLog<EntryPointV0_6_0Contract_UserOperationEventEvent_eventArgs>,
-  context: EntryPointV0_6_0Contract_UserOperationEventEvent_handlerContext
+async function handleUserOperationEvent(
+  event: eventLog<EntryPointV0_6_0_UserOperationEvent_eventArgs>,
+  context: handlerContext
 ) {
   const hash = event.params.userOpHash;
-  const bundler = event.txOrigin ? event.txOrigin.toLowerCase() : undefined;
+  // @ts-ignore
+  const bundler = event.transaction.sender
+    ? // @ts-ignore
+      event.transaction.sender.toLowerCase()
+    : undefined;
 
   context.UserOp.set({
     id: `${event.chainId}-${hash}`,
     chainId: event.chainId,
-    blockNumber: event.blockNumber,
-    blockTimestamp: event.blockTimestamp,
-    transactionIndex: event.transactionIndex,
-    transactionHash: event.transactionHash,
+    blockNumber: event.block.number,
+    blockTimestamp: event.block.timestamp,
+    // @ts-ignore
+    transactionIndex: event.transaction.index,
+    // @ts-ignore
+    transactionHash: event.transaction.hash,
     hash,
     actualGasCost: event.params.actualGasCost,
     actualGasUsed: event.params.actualGasUsed,
--- src/kernelV3Account.ts
+++ src/kernelV3Account.ts
@@ -1,83 +1,77 @@
-import { KernelV3AccountContract } from "../generated/src/Handlers.gen";
+import { KernelV3Account } from "generated";
 
 const TYPE_VALIDATOR = "0x01";
 const TYPE_PERMISSION = "0x02";
 
-KernelV3AccountContract.RootValidatorUpdated.loader(({ event, context }) => {
-  const account = event.srcAddress.toLowerCase();
-  context.KernelV3Account.load(`${event.chainId}-${account}`);
+KernelV3Account.RootValidatorUpdated.handlerWithLoader({
+  loader: ({ event, context }) => {
+    const account = event.srcAddress.toLowerCase();
+    return context.KernelV3Account.get(`${event.chainId}-${account}`);
+  },
+  handler: async ({ event, context, loaderReturn: account }) => {
+    const accountAddress = event.srcAddress.toLowerCase();
+    if (!account) {
+      return;
+    }
+    const rootValidator = event.params.rootValidator.toLowerCase();
+    const validatorType = rootValidator.slice(0, 4);
+    if (validatorType === TYPE_VALIDATOR) {
+      const validatorAddress = `0x${rootValidator.slice(4, 44)}`;
+      context.KernelV3Account.set({
+        id: `${event.chainId}-${accountAddress}`,
+        modules: account.modules,
+        chainId: account.chainId,
+        rootValidator: validatorAddress,
+        address: account.address,
+      });
+    } else if (validatorType === TYPE_PERMISSION) {
+      return;
+    }
+  },
 });
 
-KernelV3AccountContract.RootValidatorUpdated.handler(({ event, context }) => {
-  const accountAddress = event.srcAddress.toLowerCase();
-  const account = context.KernelV3Account.get(
-    `${event.chainId}-${accountAddress}`
-  );
-  if (!account) {
-    return;
-  }
-  const rootValidator = event.params.rootValidator.toLowerCase();
-  const validatorType = rootValidator.slice(0, 4);
-  if (validatorType === TYPE_VALIDATOR) {
-    const validatorAddress = `0x${rootValidator.slice(4, 44)}`;
+KernelV3Account.ModuleInstalled.handlerWithLoader({
+  loader: ({ event, context }) => {
+    const account = event.srcAddress.toLowerCase();
+    return context.KernelV3Account.get(`${event.chainId}-${account}`);
+  },
+  handler: async ({ event, context, loaderReturn: account }) => {
+    const accountAddress = event.srcAddress.toLowerCase();
+    if (!account) {
+      return;
+    }
+    const module = event.params.mod.toLowerCase();
+    const modules = account.modules;
+    modules.push(module);
     context.KernelV3Account.set({
       id: `${event.chainId}-${accountAddress}`,
-      modules: account.modules,
+      modules,
       chainId: account.chainId,
-      rootValidator: validatorAddress,
+      rootValidator: account.rootValidator,
       address: account.address,
     });
-  } else if (validatorType === TYPE_PERMISSION) {
-    return;
-  }
+  },
 });
 
-KernelV3AccountContract.ModuleInstalled.loader(({ event, context }) => {
-  const account = event.srcAddress.toLowerCase();
-  context.KernelV3Account.load(`${event.chainId}-${account}`);
-});
-
-KernelV3AccountContract.ModuleInstalled.handler(({ event, context }) => {
-  const accountAddress = event.srcAddress.toLowerCase();
-  const account = context.KernelV3Account.get(
-    `${event.chainId}-${accountAddress}`
-  );
-  if (!account) {
-    return;
-  }
-  const module = event.params.mod.toLowerCase();
-  const modules = account.modules;
-  modules.push(module);
-  context.KernelV3Account.set({
-    id: `${event.chainId}-${accountAddress}`,
-    modules,
-    chainId: account.chainId,
-    rootValidator: account.rootValidator,
-    address: account.address,
-  });
-});
-
-KernelV3AccountContract.ModuleUninstalled.loader(({ event, context }) => {
-  const account = event.srcAddress.toLowerCase();
-  context.KernelV3Account.load(`${event.chainId}-${account}`);
-});
-
-KernelV3AccountContract.ModuleUninstalled.handler(({ event, context }) => {
-  const accountAddress = event.srcAddress.toLowerCase();
-  const account = context.KernelV3Account.get(
-    `${event.chainId}-${accountAddress}`
-  );
-  if (!account) {
-    return;
-  }
-  const module = event.params.mod.toLowerCase();
-  const modules = account.modules;
-  modules.splice(modules.indexOf(module), 1);
-  context.KernelV3Account.set({
-    id: `${event.chainId}-${accountAddress}`,
-    modules,
-    chainId: account.chainId,
-    rootValidator: account.rootValidator,
-    address: account.address,
-  });
+KernelV3Account.ModuleUninstalled.handlerWithLoader({
+  loader: ({ event, context }) => {
+    const account = event.srcAddress.toLowerCase();
+    return context.KernelV3Account.get(`${event.chainId}-${account}`);
+  },
+  handler: async ({ event, context, loaderReturn: account }) => {
+    const accountAddress = event.srcAddress.toLowerCase();
+    if (!account) {
+      return;
+    }
+    const module = event.params.mod.toLowerCase();
+    const modules = account.modules;
+    modules.splice(modules.indexOf(module), 1);
+    context.KernelV3Account.set({
+      id: `${event.chainId}-${accountAddress}`,
+      modules,
+      chainId: account.chainId,
+      rootValidator: account.rootValidator,
+      address: account.address,
+    });
+  },
 });
