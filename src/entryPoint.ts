b6d6f364facbc13b9857f5e3a1b6f7cff9920b93
Timur Badretdinov <accounts@destiner.io>
2024-11-19 19:06:43 +0100

    feat: remove EP 0.6 support

--- src/entryPoint.ts
+++ src/entryPoint.ts
@@ -1,53 +1,16 @@
 import {
-  contractRegistrations,
-  EntryPointV0_6_0,
-  EntryPointV0_6_0_AccountDeployed_eventArgs,
-  EntryPointV0_6_0_UserOperationEvent_eventArgs,
+  EntryPointV0_7_0_UserOperationEvent_eventArgs,
   EntryPointV0_7_0,
   eventLog,
   handlerContext,
 } from "generated";
-
-EntryPointV0_6_0.AccountDeployed.contractRegister(({ event, context }) => {
-  handleAccountDeployedContractRegister(event, context);
-});
-EntryPointV0_7_0.AccountDeployed.contractRegister(({ event, context }) => {
-  handleAccountDeployedContractRegister(event, context);
-});
-
-function handleAccountDeployedContractRegister(
-  event: eventLog<EntryPointV0_6_0_AccountDeployed_eventArgs>,
-  context: contractRegistrations
-) {
-  const address = event.params.sender.toLowerCase();
-  const factory = event.params.factory.toLowerCase();
-}
-
-EntryPointV0_6_0.AccountDeployed.handler(async ({ event, context }) => {
-  handleAccountDeployedEvent(event, context);
-});
-EntryPointV0_7_0.AccountDeployed.handler(async ({ event, context }) => {
-  handleAccountDeployedEvent(event, context);
-});
-
-async function handleAccountDeployedEvent(
-  event: eventLog<EntryPointV0_6_0_AccountDeployed_eventArgs>,
-  context: handlerContext
-) {
-  const address = event.params.sender.toLowerCase();
-  const factory = event.params.factory.toLowerCase();
-}
-
-EntryPointV0_6_0.UserOperationEvent.handler(async ({ event, context }) => {
-  handleUserOperationEvent(event, context);
-});
 
 EntryPointV0_7_0.UserOperationEvent.handler(async ({ event, context }) => {
   handleUserOperationEvent(event, context);
 });
 
 async function handleUserOperationEvent(
-  event: eventLog<EntryPointV0_6_0_UserOperationEvent_eventArgs>,
+  event: eventLog<EntryPointV0_7_0_UserOperationEvent_eventArgs>,
   context: handlerContext
 ) {
   const hash = event.params.userOpHash;
